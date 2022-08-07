9555aef045dae5bfc9f1a2b6f049d1bac87387e9
kan <kevin.chong.an@gmail.com>
2022-08-07 19:52:43 -0400

    Delete main.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,49 @@
 import { useState } from "react"
 import { createModuleResolutionCache } from "typescript"
-import { Keyboard} from "./components/keyboard/keyboard"
+import { Keyboard } from "./components/keyboard/keyboard"
+import { kGuessesSet } from "./components/constants/guesses";
+import "./generated_css/output.css"
+import { kCandidates, kCandidatesSet } from "./components/constants/candidates";
+
+const kWordLength = 5;
+const kGuessAttempts = 6;
 
 function App() {
+    console.log("running");
     const [guesses, setGuesses] = useState<string[]>([]);
     const [currentGuess, setCurrentGuess] = useState("");
-  
+    const [word] = useState<string>(kCandidates[Math.floor(Math.random() * kCandidates.length)]);
+    console.log("word: " + word);
     const onLetter = (value: string) => {
-      if (currentGuess.length < 5) {
-        setCurrentGuess(`${currentGuess}${value}`);
-      }
+        if (currentGuess.length < 5) {
+            setCurrentGuess(`${currentGuess}${value.toLowerCase()}`);
+        } 
     };
-  
+
     const onDelete = () => {
-      setCurrentGuess(currentGuess.slice(0, -1));
+        setCurrentGuess(currentGuess.slice(0, -1));
     };
-  
+
     const onEnter = () => {
-      // TODO: check if the current guess is in the words list
-      if (currentGuess.length === 5 && guesses.length < 6) {
-        setGuesses([...guesses, currentGuess]);
-        setCurrentGuess("");
-      }
+        if (!kGuessesSet.has(currentGuess) && !kCandidatesSet.has(currentGuess)) {
+            return console.error("not a word");
+        }
+        if (currentGuess.length === kWordLength && guesses.length < kGuessAttempts) {
+            if (currentGuess === word) {
+                return console.log("You won!");
+            }
+            setGuesses([...guesses, currentGuess]);
+            setCurrentGuess("");
+        }
     };
     console.log(currentGuess);
     console.log(guesses);
-  
+
     return (
-      <div>
-        <Keyboard onLetter={onLetter} onDelete={onDelete} onEnter={onEnter} />
-      </div>
+        <div>
+            <Keyboard onLetter={onLetter} onDelete={onDelete} onEnter={onEnter} />
+        </div>
     );
-  }
-  
-  export default App
+}
+
+export default App
--- src/components/keyboard/keyboard.tsx
+++ src/components/keyboard/keyboard.tsx
@@ -1,5 +1,6 @@
 import { KeyValue, KeysByRow, LetterStatus, getWidth } from "../constants/keyboard";
 import { KeyboardKey } from "./keyboardKey";
+import { useEffect } from "react";
 
 type Props = {
     onDelete: () => void;
@@ -7,8 +8,8 @@ type Props = {
     onLetter: (value: string) => void;
 }
 
-export const Keyboard = (props : Props) => {
-    const onClick = (value : KeyValue) => {
+export const Keyboard = (props: Props) => {
+    const onClick = (value: KeyValue) => {
         if (value === "ENTER") {
             return props.onEnter();
         }
@@ -18,11 +19,36 @@ export const Keyboard = (props : Props) => {
         return props.onLetter(value);
     }
 
+    useEffect(() => {
+        const listener = (e: KeyboardEvent) => {
+            if (e.code === "Enter") {
+                props.onEnter();
+            } else if (e.code === "Backspace") {
+                props.onDelete();
+            } else {
+                const key = e.key.toUpperCase()
+                if (key.length === 1 && key >= 'A' && key <= 'Z') {
+                    props.onLetter(key);
+                }
+            }
+        }
+        window.addEventListener('keyup', listener)
+        return () => {
+            window.removeEventListener('keyup', listener);
+        }
+    }, [props])
+
     const renderKeyboard = () => {
         return KeysByRow.map((row, i) => {
-            return (<div className={"flex justify-center mb-1"} >{
-                row.map(keyValue => {
-                    return (<KeyboardKey value={keyValue} width={getWidth(keyValue)} onClick={onClick}>{keyValue}</KeyboardKey>);
+            return (<div key={i} className={"flex justify-center mb-1"} >{
+                row.map((keyValue, j) => {
+                    return (<
+                        KeyboardKey key={j} 
+                        value={keyValue} 
+                        width={getWidth(keyValue)} 
+                        onClick={onClick}>
+                            {keyValue === "DELETE" ? "⌫" : keyValue === "ENTER" ? "→" : keyValue}
+                        </KeyboardKey>);
                 })}
             </div>);
         })
